dd7d59db49e02a6ea3cfcd538940af3d7d56c281
Jaime Kvaternik <jaime.kvaternik17@gmail.com>
2020-04-25 16:01:35 -0700

    add new files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jk-portfolio';
+  title = 'Jaime Kvaternik | CS + Design Student';
 }
